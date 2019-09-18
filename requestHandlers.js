efd353f2f433c8d89e9512187bb790dcfbcd11c5
jcrt <julio.rosatrindade@gmail.com>
2019-09-17 21:16:59 -0300

    Fim do trabalho

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,8 +2,9 @@ const urlModule = require("url");
 const queryStrModule = require("querystring");
 const chessModule = require("./chess");
 
-//Action: GET
+//Action
 function hello(request, response){
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     response.write("<h1>DCC195 - Laboratório de sistemas web III</h1>");
     response.write("<ol>")
     response.write("<li><a href='/sobre.html'>Sobre</a></li>");
@@ -12,18 +13,21 @@ function hello(request, response){
     response.write("<li><a href='/equacao.html'>Equação do segundo grau</a></li>");
     response.write("<li><a href='/xadrez.html'>Xadrez</a></li>");
     response.write("</ol>");
+    response.end();
 }
 
-//Action: GET
+//Action
 function sobre(request, response){
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     response.write("<h1>Sobre o aluno</h1>");
     response.write("<b>Nome: </b> Julio Cesar Rosa Trindade<br />");
     response.write("<b>Matrícula: </b> 201376082<br />");
     response.write("<b>E-mail: </b> jtrindade@ice.ufjf.br<br />");
     response.write("<b>Curso: </b> Bacharelado em Sistemas de Informação<br />");
+    response.end();
 }
 
-//Action: GET
+//Action
 function aleatorios(request, response){
     let range = 100;
     let listaPar = [];
@@ -38,17 +42,19 @@ function aleatorios(request, response){
             listaImpar.push(number);
         }
     }
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     response.write("<table><tr><td style='vertical-align: top'>");
     response.write(printList("Lista de números pares", listaPar));
     response.write("</td><td style='vertical-align: top'>");
     response.write(printList("Lista de números ímpares", listaImpar));
     response.write("</td></tr></table>");
+    response.end();
 }
 
-//Action: GET
+//Action
 function primos(request, response){
     let primosQueryStr = getQueryString(request.url);
-    
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     if(isParamPrimosValido(primosQueryStr.inicio) && isParamPrimosValido(primosQueryStr.fim) && primosQueryStr.inicio <= primosQueryStr.fim){
         response.write(
             printList(
@@ -59,50 +65,49 @@ function primos(request, response){
     } else {
         response.write(`Os parâmetros para inicio=${primosQueryStr.inicio} e fim=${primosQueryStr.fim} informados são inválidos`);
     }
+    response.end();
 }
 
-//Action: GET e POST
+//Action
 function equacao(request, response){
     if(request.method == "GET"){
+        response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
         response.write("<form method='POST' action='/equacao.html'>");
         response.write("<p><input name='a' type='text' placeholder='a' style='width: 40px' />x² + ");
         response.write("<input name='b' type='text' placeholder='b' style='width: 40px'  />x + ");
         response.write("<input name='c' type='text' placeholder='c' style='width: 40px'  /> = 0</p>");
         response.write("<p><input name='Calcular' type='submit' value='submit' /></p>");
         response.write("</form>");
+        response.end();
     }
     else if(request.method == "POST"){    
-        
-        // let bodyStr = '';
-        // request.on('data', (chunk) => {
-        //     bodyStr += chunk;
-        // }).on('end', () => {
-        //     console.log(bodyStr);
-        // });
-
-        equacao = {
-            "a": 1, 
-            "b": -3, 
-            "c": -10
-        };
-
-        if(isParamEquacaoValido(equacao.a) && isParamEquacaoValido(equacao.b) && isParamEquacaoValido(equacao.c)){
-            let raizes = calcRaizesEquacao(parseInt(equacao.a), parseInt(equacao.b), parseInt(equacao.c));
-            if(raizes["x1"] != undefined){
-                response.write(`Esta equação tem raiz x1 = ${raizes["x1"]}`);
-                if(raizes["x2"] != undefined)
-                    response.write(` e x2 = ${raizes["x2"]}`);
-            } else{
-                response.write("Esta equação não tem raízes reais");
+        let bodyStr='?';
+        request.on("data", (data) =>{
+            bodyStr += data.toString();
+        }).on("end", () => {
+            let equacao = getQueryString(bodyStr);
+            response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
+            if(isParamEquacaoValido(equacao.a) && isParamEquacaoValido(equacao.b) && isParamEquacaoValido(equacao.c)){
+                let raizes = calcRaizesEquacao(parseInt(equacao.a), parseInt(equacao.b), parseInt(equacao.c));
+                if(raizes["x1"] != undefined){
+                    response.write(`Esta equação tem raiz x1 = ${raizes["x1"]}`);
+                    if(raizes["x2"] != undefined)
+                        response.write(` e x2 = ${raizes["x2"]}`);
+                } else{
+                    response.write("Esta equação não tem raízes reais");
+                }
             }
-        }
-        else{
-            response.write(`Os valores para a=${equacao.a}, b=${equacao.b} e c=${equacao.c} informados são inválidos`);
-        }
+            else{
+                response.write(`Os valores para a=${equacao.a}, b=${equacao.b} e c=${equacao.c} informados são inválidos`);
+            }
+            response.end();
+        });      
     }
 }
 
+//Action
 function xadrez(request, response){
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     let coords = getQueryString(request.url);
     let x = 0, y = 0;
     if(coords.coluna != undefined){
@@ -112,9 +117,12 @@ function xadrez(request, response){
         y = coords.linha;
     }
     response.write(chessModule.drawBoard(x, y));
+    response.end();
 }
 
+//Action
 function getJsonHorse(request, response){
+    response.writeHead(200, {"Content-Type": `application/json; charset=utf-8`});
     let coords = getQueryString(request.url);
     let x = 0, y = 0;
     if(coords.coluna != undefined){
@@ -124,11 +132,14 @@ function getJsonHorse(request, response){
         y = coords.linha;
     }
     response.write(chessModule.getJsonHorse(x, y));
+    response.end();
 }
 
-//Action: GET
+//Action
 function error(request, response){
+    response.writeHead(200, {"Content-Type": `text/html; charset=utf-8`});
     response.write("<h1>Esta página que está procurando não existe   =( </h1>");
+    response.end();
 }
 
 function isParamPrimosValido(param){
@@ -201,10 +212,10 @@ function calcRaizesEquacao(a, b, c){
     let delta = calcDelta(a, b, c);
 
     if(delta == 0){
-        results["x1"] = calcBhaskara(a, b, delta, false);
+        results["x1"] = calcBhaskara(a, b, delta, false).toFixed(2);
     } else if(delta > 0){
-        results["x1"] = calcBhaskara(a, b, delta, false);
-        results["x2"] = calcBhaskara(a, b, delta, true);
+        results["x1"] = calcBhaskara(a, b, delta, false).toFixed(2);
+        results["x2"] = calcBhaskara(a, b, delta, true).toFixed(2);
     }
     return results;
 }
--- router.js
+++ router.js
@@ -1,17 +1,9 @@
 function route (pathname, handlers, request, response){
-    
-    let contentType = (handlers[pathname]["contentType"] == undefined ? 'text/html': handlers[pathname]["contentType"]);
-    response.writeHead(200, {
-        "Content-Type": `${contentType}; charset=utf-8`, 
-    });
-
     if(typeof handlers[pathname] == 'function'){
         handlers[pathname](request, response);
     } else {
        handlers["error"](request, response);
     }
-
-    response.end();
 }
 
 exports.route = route;
